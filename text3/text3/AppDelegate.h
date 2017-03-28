d768adca98b3883f0fa4a78108017bf04e65b17a
LGG <LGG@163.com>
2017-03-28 16:59:27 +0800

    初始化项目

---
+++ text3/text3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text3
+//
+//  Created by 郑连乐 on 2017/3/28.
+//  Copyright © 2017年 beijingshuoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text3/text3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text3
+//
+//  Created by 郑连乐 on 2017/3/28.
+//  Copyright © 2017年 beijingshuoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
